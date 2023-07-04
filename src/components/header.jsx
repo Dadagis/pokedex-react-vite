4a87975f624a2ce2be7e2cbead3b64ddad64802b
David-Alexandre Jungblut <58357600+Dadagis@users.noreply.github.com>
2023-07-04 21:31:24 +0200

    :fire: Card test file removed

---
+++ src/components/header.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+function header(props) {
+  return (
+    <div>
+      <p>Pokedex</p>
+    </div>
+  );
+}
+
+header.propTypes = {};
+
+export default header;
