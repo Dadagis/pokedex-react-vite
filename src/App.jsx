168f9716e1065504a72c5cb949136d40382a2b5b
David-Alexandre Jungblut <58357600+Dadagis@users.noreply.github.com>
2023-07-14 20:22:38 +0200

    :recycle: Moved page logic outside cardsList

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,22 @@
 import './App.css';
 import Header from './components/header';
 import CardsList from './components/cardsList';
+import { useState } from 'react';
 
 function App() {
+  const [page, setPage] = useState(1);
+
+  const updatePage = (increment = true) => {
+    if (page === 1 && !increment) return;
+
+    setPage((prevPage) => (increment ? prevPage + 1 : prevPage - 1));
+  };
+
   return (
     <>
       <Header></Header>
-      <CardsList></CardsList>
+
+      <CardsList page={page} updatePage={updatePage}></CardsList>
     </>
   );
 }
--- src/components/cardsList.jsx
+++ src/components/cardsList.jsx
@@ -3,8 +3,7 @@ import PropTypes from 'prop-types';
 import Card from './card';
 import '../styles/o-cardsList/o-cardsList.css';
 import Button from './button';
-function cardsList() {
-  const [page, setPage] = useState(1);
+function cardsList({ page, updatePage }) {
   const [base, setBase] = useState(21);
   const [ids, setIds] = useState([...Array(base).keys()]);
 
@@ -15,12 +14,6 @@ function cardsList() {
     }
     setIds(newIds);
   }, [page]);
-
-  const updatePage = (increment = true) => {
-    if (page === 1 && !increment) return;
-
-    setPage((prevPage) => (increment ? prevPage + 1 : prevPage - 1));
-  };
 
   const displayCards = () => {
     return ids.map((index) => <Card key={index} id={index + 1} />);
