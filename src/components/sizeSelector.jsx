053f3a1cb03f597d28f3920e37894b5c4cc981e3
David-Alexandre Jungblut <58357600+Dadagis@users.noreply.github.com>
2023-07-22 14:00:53 +0200

    :dizzy: Super painful transition state

--- src/components/sizeSelector.jsx
+++ src/components/sizeSelector.jsx
@@ -1,66 +1,80 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import '../styles/m-sizeSelector/m-sizeSelector.css';
 
 function sizeSelector({ handleClick }) {
+  const toggleSelected = ({ target }) => {
+    if (target.classList.contains('--selected')) return;
+
+    const parent = target.parentElement;
+    parent.classList.toggle('--large');
+    parent.querySelectorAll('.m-sizeSelector__option').forEach((element) => {
+      element.classList.toggle('--selected');
+    });
+  };
+
   return (
-    <div>
-      <div>
-        <svg
-          width="24"
-          height="24"
-          viewBox="0 0 24 24"
-          fill="none"
-          xmlns="http://www.w3.org/2000/svg"
-          onClick={() => handleClick(false)}
-        >
-          <rect width="24" height="24" rx="2" fill="#D9D9D9" />
-          <rect
-            x="3.36002"
-            y="2.88"
-            width="7.2"
-            height="7.2"
-            rx="1"
-            fill="#3D3D3D"
-          />
-          <rect
-            x="3.36002"
-            y="13.44"
-            width="7.2"
-            height="7.2"
-            rx="1"
-            fill="#3D3D3D"
-          />
-          <rect
-            x="13.92"
-            y="2.88"
-            width="7.2"
-            height="7.2"
-            rx="1"
-            fill="#3D3D3D"
-          />
-          <rect
-            x="13.92"
-            y="13.44"
-            width="7.2"
-            height="7.2"
-            rx="1"
-            fill="#3D3D3D"
-          />
-        </svg>
-      </div>
-      <div>
-        <svg
-          width="24"
-          height="24"
-          viewBox="0 0 24 24"
-          fill="none"
-          xmlns="http://www.w3.org/2000/svg"
-          onClick={() => handleClick(true)}
-        >
-          <rect width="24" height="24" rx="2" fill="#D9D9D9" />
-          <rect x="2" y="8" width="20" height="7.2" rx="1" fill="#3D3D3D" />
-        </svg>
-      </div>
+    <div className="m-sizeSelector">
+      <svg
+        width="19"
+        height="19"
+        viewBox="0 0 19 19"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+        onClick={(event) => {
+          handleClick(false);
+          toggleSelected(event);
+        }}
+        className="m-sizeSelector__option --selected"
+      >
+        <rect
+          x="0.360016"
+          y="0.880005"
+          width="7.2"
+          height="7.2"
+          rx="1"
+          fill="#3D3D3D"
+        />
+        <rect
+          x="0.360016"
+          y="11.44"
+          width="7.2"
+          height="7.2"
+          rx="1"
+          fill="#3D3D3D"
+        />
+        <rect
+          x="10.92"
+          y="0.880005"
+          width="7.2"
+          height="7.2"
+          rx="1"
+          fill="#3D3D3D"
+        />
+        <rect
+          x="10.92"
+          y="11.44"
+          width="7.2"
+          height="7.2"
+          rx="1"
+          fill="#3D3D3D"
+        />
+      </svg>
+
+      <svg
+        width="20"
+        height="15"
+        viewBox="0 0 20 15"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+        onClick={(event) => {
+          handleClick(true);
+          toggleSelected(event);
+        }}
+        className="m-sizeSelector__option"
+      >
+        <rect width="20" height="15" rx="1" fill="#3D3D3D" />
+      </svg>
     </div>
   );
 }
