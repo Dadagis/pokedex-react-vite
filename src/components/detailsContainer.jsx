8e7698e33b73c85d84d7bf4f2634707dd0b3932f
David-Alexandre Jungblut <58357600+Dadagis@users.noreply.github.com>
2023-08-17 19:58:58 +0200

    :lipstick: Prettier display of sprite and types

--- src/components/detailsContainer.jsx
+++ src/components/detailsContainer.jsx
@@ -1,8 +1,15 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import LabelPill from './labelPill';
 import '../styles/o-detailsContainer/o-detailsContainer.css';
 
 function detailsContainer({ pokemon }) {
+  const displayTypes = () => {
+    return pokemon.types.map((type, index) => {
+      return <LabelPill key={index} type={type} has_color={true} />;
+    });
+  };
+
   return (
     <div className="o-detailsContainer">
       <div
@@ -13,7 +20,7 @@ function detailsContainer({ pokemon }) {
         <p className="a-text --name">{pokemon.name}</p>
         <img src={pokemon.sprite} alt={`Sprite of ${pokemon.name}`} />
       </div>
-      <p>{pokemon.types}</p>
+      <div className="o-detailsContainer__types">{displayTypes()}</div>
       <div className="o-detailsContainer__stats">
         <p>{pokemon.height}</p>
         <p>{pokemon.weight}</p>
--- src/components/labelPill.jsx
+++ src/components/labelPill.jsx
@@ -2,14 +2,19 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import '../styles/a-labelPill/a-labelPill.css';
 
-function labelPill({ type, ...props }) {
+function labelPill({ type, has_color, ...props }) {
+  const colored = has_color ? '--colored' : null;
   return (
-    <p key={props.key} className="a-labelPill">
+    <p
+      key={props.key}
+      className={['a-labelPill', `--${type}`, colored].join(' ').trim()}
+    >
       {type}
     </p>
   );
 }
 
-labelPill.propTypes = { type: PropTypes.string };
+labelPill.propTypes = { type: PropTypes.string, has_color: PropTypes.bool };
+labelPill.defaultProps = { type: 'fire', has_color: false };
 
 export default labelPill;
