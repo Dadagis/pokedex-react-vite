2009b4bf721135bcde2b8b0e31a63447d79e2738
David-Alexandre Jungblut <58357600+Dadagis@users.noreply.github.com>
2023-08-17 19:36:39 +0200

    :construction: Basic setup of detailsContainer card

--- src/components/detailsContainer.jsx
+++ src/components/detailsContainer.jsx
@@ -1,14 +1,23 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import '../styles/o-detailsContainer/o-detailsContainer.css';
 
 function detailsContainer({ pokemon }) {
   return (
-    <div>
-      <p>{pokemon.name}</p>
-      <img src={pokemon.sprite} alt={`Sprite of ${pokemon.name}`} />
-      <p>{pokemon.height}</p>
-      <p>{pokemon.weight}</p>
+    <div className="o-detailsContainer">
+      <div
+        className={['o-detailsContainer__header', `--${pokemon.types[0]}`]
+          .join(' ')
+          .trim()}
+      >
+        <p className="a-text --name">{pokemon.name}</p>
+        <img src={pokemon.sprite} alt={`Sprite of ${pokemon.name}`} />
+      </div>
       <p>{pokemon.types}</p>
+      <div className="o-detailsContainer__stats">
+        <p>{pokemon.height}</p>
+        <p>{pokemon.weight}</p>
+      </div>
     </div>
   );
 }
